828801e887be674865cb5896031314bf736db4a0
Chris Birk <cmbirk@gmail.com>
2012-10-22 15:54:10 -0400

    clean up config file

--- inc/config.php
+++ inc/config.php
@@ -6,42 +6,49 @@
  *	@license http://www.gnu.org/licenses/ GNU GPL v.3
  */
 
-	/* SERVER DEFINITIONS
-	=====================================================================*/
+	/** 
+	 *	Site Configurations
+	 */
+	define('DB_HOST', '');
+	define('DB_NAME', '');
+	define('DB_USER', '');
+	define('DB_PASS', '');
+
+	/**
+	 *	Server Definitions
+	 */
 	define('SERVER_ABS', $_SERVER['DOCUMENT_ROOT']);
 	define('SERVER_URL', 'http://'.$_SERVER['HTTP_HOST']);
-	define('IS_STAGE', 	 strpos($_SERVER['HTTP_HOST'], '.yourdomain.com') !== false);
 
-	/* MISC DEFINITIONS
-	* TODO: Use server email with option for SMTP
-	=====================================================================*/
+	/** 
+	 * Postmark Definitions
+	 */
 	define('POSTMARK_APIKEY', 	'');
 	define('POSTMARK_EMAIL',  	'');
 	define('POSTMARK_NAME',		'');
 
-	#ERRORS
+	/**
+	 *	Error Reporting Settings
+	 */ 
 	ini_set('log_errors', 'on');
 	ini_set('display_errors', 'off');
 	ini_set('display_startup_errors', 'off');
 	ini_set('error_log', '');
-		
-	#SESSION LOCATION
-	//ini_set('session.save_path', '');
-		
-		
-	$db_creds = array(
-		'rw'=>array('host'=>'localhost', 'name'=>'', 'user'=>'', 'pass'=>'')
-		//,'r2'=>array('host'=>'', 'name'=>'', 'user'=>'', 'pass'=>'')
-	);
 	
-	//define('DB_PREFIX', 'hoba_'); //DB TABLE PREFIX
+	/**
+	 * 	Database Name Definitions
+	 */
 	define('DB_PREFIX', '');
-	 
-	/* DB TABLES NAME DEFINITIONS
-	=====================================================================*/
 	define('DB_TBL_BILLS',			DB_PREFIX.'bills');
 	define('DB_TBL_BILL_CONTENT',	DB_PREFIX.'bill_content');
 	define('DB_TBL_NOTES',			DB_PREFIX.'notes');
 	define('DB_TBL_TOP_NOTES',		DB_PREFIX.'top_notes');
 	define('DB_TBL_USERS', 			DB_PREFIX.'users');
 	define('DB_TBL_USER_META', 		DB_PREFIX.'user_meta');
+	
+	/**
+	 * 	Database Credentials Array
+	 */
+	$db_creds = array(
+		'rw'=>array('host'=>DB_HOST, 'name'=>DB_NAME, 'user'=>DB_USER, 'pass'=>DB_PASS)
+	);
