26186b6fc48efad6f3c9c935ec2dc97aef49139e
Chris Birk <cmbirk@gmail.com>
2012-10-22 15:49:14 -0400

    fix jquery.php json responses breaking when not encoded as utf8

--- inc/jquery.php
+++ inc/jquery.php
@@ -114,6 +114,25 @@
 		$_SESSION['bill'] = $b;
 	}
 	
+	function utf8_encode_deep(&$input) {
+	    if (is_string($input)) {
+	        $input = utf8_encode($input);
+	    } else if (is_array($input)) {
+	        foreach ($input as &$value) {
+	            utf8_encode_deep($value);
+	        }
+
+	        unset($value);
+	    } else if (is_object($input)) {
+	        $vars = array_keys(get_object_vars($input));
+
+	        foreach ($vars as $var) {
+	            utf8_encode_deep($input->$var);
+	        }
+	    }
+	}
+	
+	utf8_encode_deep($response);
 	echo json_encode($response);
 	
 ?>
