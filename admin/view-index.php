b62a7eb5a1d26193a2d6d6b27d584ee485e8de4c
Chris Birk <cmbirk@gmail.com>
2013-01-16 22:46:45 -0500

    comment unbuilt admin pages

--- admin/view-index.php
+++ admin/view-index.php
@@ -8,11 +8,11 @@
 ?>
 <h1>Administrative Index</h1>
 <ul>
-	<li><a href="/admin/info">Site Info</a></li>
+	<!--<li><a href="/admin/info">Site Info</a></li>-->
 	<li><a href="/admin/nav">Edit Navigation Bar</a></li>
 	<li><a href="/admin/company-approval">Company Approvals</a></li>
-	<li><a href="/admin/users">Moderate Users</a></li>
+	<!--<li><a href="/admin/users">Moderate Users</a></li>
 	<li><a href="/admin/notes">Moderate Notes</a></li>
-	<li><a href="/admin/import">Import Documents</a></li>
+	<li><a href="/admin/import">Import Documents</a></li>-->
 	<li><a href="/admin/docs">View/Edit Documents</a></li>
 </ul>
