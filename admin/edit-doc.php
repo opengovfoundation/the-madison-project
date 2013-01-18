3414dcc10ed4320280104c004ef0dac21824181a
Chris Birk <cmbirk@gmail.com>
2013-01-18 11:58:23 -0500

    start document editing

--- admin/edit-doc.php
+++ admin/edit-doc.php
@@ -28,20 +28,28 @@
 ?>
 <a href="/admin/docs"><< Back</a>
 <h1><?php echo $b->title; ?></h1>
-<h2>Document Info:</h2>
-<form action="" method="post" name="doc_info">
-	<label for="title">Title:</label>
-	<input type="text" name="title" value="<?php echo $b->title; ?>"/>
-	<label for="shortname">Shortname:</label>
-	<input type="text" name="shortname" value="<?php echo $b->shortname; ?>" />
-	<label for="twitter_text">Twitter Text:</label>
-	<input type="text" name="twitter_text" value="<?php echo $b->twitter_text; ?>"/>
-	<label for="twitter_hash">Twitter Hashtags:</label>
-	<input type="text" name="twitter_hash" value="<?php echo $b->twitter_hash; ?>"/>
-	<label for="doc_location">Document Location:</label>
-	<input type="text" name="doc_location" value="<?php echo $b->doc_location; ?>"/>
-	<label for="description">Description:</label>
-	<textarea name="description" id="description" cols="50" rows="20"><?php echo $b->description; ?></textarea>
-	<input type="hidden" value="bill_info" name="form_submission"/>
-	<input type="submit" value="Update"/>
-</form>
+<div class="edit_doc_info">
+	<h2>Document Info:</h2>
+	<form action="" method="post" name="doc_info">
+		<label for="title">Title:</label>
+		<input type="text" name="title" value="<?php echo $b->title; ?>"/>
+		<label for="shortname">Shortname:</label>
+		<input type="text" name="shortname" value="<?php echo $b->shortname; ?>" />
+		<label for="twitter_text">Twitter Text:</label>
+		<input type="text" name="twitter_text" value="<?php echo $b->twitter_text; ?>"/>
+		<label for="twitter_hash">Twitter Hashtags:</label>
+		<input type="text" name="twitter_hash" value="<?php echo $b->twitter_hash; ?>"/>
+		<label for="doc_location">Document Location:</label>
+		<input type="text" name="doc_location" value="<?php echo $b->doc_location; ?>"/>
+		<label for="description">Description:</label>
+		<textarea name="description" id="description" cols="50" rows="20"><?php echo $b->description; ?></textarea>
+		<input type="hidden" value="bill_info" name="form_submission"/>
+		<input type="submit" value="Update"/>
+	</form>
+</div>
+<div class="edit_doc_content">
+	<h2>Document Content:</h2>
+	<div class="doc_sections">
+		<?php print_r($b->content); ?>
+	</div>
+</div>
