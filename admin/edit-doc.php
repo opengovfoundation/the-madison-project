1716064389cf4311118d860d8bcddeef0b08cae4
Chris Birk <cmbirk@gmail.com>
2013-01-29 14:44:48 -0500

    added ability to add/delete/edit content on admin page

--- admin/edit-doc.php
+++ admin/edit-doc.php
@@ -35,16 +35,15 @@
 					<?php
 						$content = $parent['content'];
 						if(strlen($content) > 50){
-							$content = substr($content, 0, 50) . '...';
-							?>
-							<img style="cursor:pointer;" src="/assets/i/arrow-down.png" alt="Dropdown Arrow" class="dropdown_arrow" />
-							<?php
+							$content = substr($content, 0, 100) . '...';
 						}
 					?>
+					<img style="cursor:pointer;" src="/assets/i/arrow-down.png" alt="Dropdown Arrow" class="dropdown_arrow" />
 					<?php echo $content; ?>
 				</span>
 				<input type="hidden" value="<?php echo $parent['id']; ?>"/>
-				<p class="delete_doc_item">delete</p>
+				<p class="add_doc_item">+</p>
+				<p class="delete_doc_item">x</p>
 				<p class="doc_item_content"><textarea><?php echo $parent['content']; ?></textarea></p>
 			</div>
 		</li>
@@ -64,19 +63,20 @@
 	}
 ?>
 <a href="/admin/docs">&lt;&lt; Back</a>
-<h1><?php echo $b->title; ?></h1>
-<div class="edit_doc_info">
+<h1><?php echo $b->title; ?> </h1>
+<div class="edit_info_link">Edit Document Info</div>
+<div class="edit_doc_info hidden">
 	<h2>Document Info:</h2>
 	<form action="" method="post" name="doc_info">
 		<label for="title">Title:</label>
 		<input type="text" name="title" value="<?php echo $b->title; ?>"/>
-		<label for="shortname">Shortname:</label>
+		<label for="shortname">Shortname <img src="/assets/i/question_icon.png" alt="Info" title="Document short title used in various places in the document reader" class="admin_info_ico"/>:</label>
 		<input type="text" name="shortname" value="<?php echo $b->shortname; ?>" />
-		<label for="twitter_text">Twitter Text:</label>
+		<label for="twitter_text">Twitter Text <img src="/assets/i/question_icon.png" alt="Info" title="The text that is pre-filled in the tweet button" class="admin_info_ico"/>:</label>
 		<input type="text" name="twitter_text" value="<?php echo $b->twitter_text; ?>"/>
-		<label for="twitter_hash">Twitter Hashtags:</label>
+		<label for="twitter_hash">Twitter Hashtags <img src="/assets/i/question_icon.png" alt="Info" title="The hashes that are appended to the pre-filled tweet" class="admin_info_ico"/>:</label>
 		<input type="text" name="twitter_hash" value="<?php echo $b->twitter_hash; ?>"/>
-		<label for="doc_location">Document Location:</label>
+		<label for="doc_location">Document Location <img src="/assets/i/question_icon.png" alt="Info" title="The location on disk of the pdf for this document" class="admin_info_ico"/>:</label>
 		<input type="text" name="doc_location" value="<?php echo $b->doc_location; ?>"/>
 		<label for="description">Description:</label>
 		<textarea name="description" id="description" cols="50" rows="20"><?php echo $b->description; ?></textarea>
@@ -101,16 +101,11 @@
 			</div>
 			<?php
 			endforeach;
-			
-			
-			
-				
-			//Add ability to add new sections
-				
-			//Save the document
-		
 		?>
 	</div>
+</div>
+<div class="add_doc_content list_wrapper">
+	<h2 class="center">Add Content:</h2>
 </div>
 <script type="text/javascript">
 	$(document).ready(function(){
@@ -127,7 +122,27 @@
 		}
 	});
 	
-	$('.doc_item .dropdown_arrow').click(function(){
+	function add_doc_handler(){
+		//Create parent doc item
+		var doc_item = $('<li class="doc_item"></li>');
+		
+		//Create objects to be appended to parent doc item
+		var sib1 = $('<span></span>').append($('<img style="cursor:pointer;" src="/assets/i/arrow-down.png" alt="Dropdown Arrow" class="dropdown_arrow" />').click(dropdown_arrow_handler)).append(' New Content');
+		var sib2 = $('<input type="hidden" value="" />');
+		var sib3 = $('<p class="add_doc_item">+</p>').click(add_doc_handler);
+		var sib4 = $('<p class="delete_doc_item">x</p>').click(delete_doc_handler);
+		var sib5 = $('<p class="doc_item_content"><textarea>New Content</textarea></p>');
+		
+		//Append a div and the child elements to the parent doc item
+		doc_item.append($('<div></div>').append([sib1, sib2, sib3, sib4, sib5]));
+		
+		//Append the parent doc item to the list
+		$(this).parent('div').parent('.doc_item').after(doc_item);
+	}
+	function delete_doc_handler(){
+		$(this).parent('div').parent('.doc_item').remove();
+	}
+	function dropdown_arrow_handler(){
 		var sibling_content = $(this).parent('span').siblings('.doc_item_content');
 		
 		if(sibling_content.hasClass('expanded')){
@@ -136,9 +151,19 @@
 		else{
 			sibling_content.addClass('expanded');
 		}
-	});
+	}
 	
-	$('.delete_doc_item').click(function(){
-		$(this).parent('div').parent('.doc_item').remove();
+	$('.doc_item .dropdown_arrow').click(dropdown_arrow_handler);
+	$('.delete_doc_item').click(delete_doc_handler);
+	$('.add_doc_item').click(add_doc_handler);
+	$('.edit_info_link').click(function(){
+		if($(this).hasClass('red')){
+			$(this).removeClass('red');
+			$('.edit_doc_info').hide();
+		}
+		else{
+			$(this).addClass('red');
+			$('.edit_doc_info').show();
+		}
 	});
 </script>
